18d58769f881708ffb6151800255334901972b2f
kambains226 <kam2711@outlook.com>
2024-02-03 21:54:00 +0000

    working on the who starts and made it so when its me the user starts

--- ticTacToe.py
+++ ticTacToe.py
@@ -6,11 +6,12 @@ this python file acts as the interface
 for the ticTacToe minigame for our
 python minigame hub.
 """
-
+current='❌'
 def ticTacToe():
     #-------------------Libraries-------------------
     import customtkinter as ctk
-    
+    import time
+    import random 
     #-------------------CTk appearance-------------------
     ctk.set_appearance_mode("Dark")
     ctk.set_default_color_theme("green") 
@@ -27,27 +28,35 @@ def ticTacToe():
     tic.title('TicTacToe')
     tic.iconbitmap('Resources/iconbitmap.ico')
 
-
+    #-------------------global variables-------------------
+    
 
     #-------------------All functions go in below-------------------
 
 
     # Function to process click
-    def on_click (button_id):
+    def on_click (button_id,player):
+        
         # buttons[button_id].configure(state=ctk.DISABLED)
         buttons[button_id].configure(state=ctk.DISABLED)
         print(f"button{button_id+1} pressed")
-        user()
-        buttons[button_id].configure(text=button_symbols[button_id])
+        # user()
+        
+        buttons[button_id].configure(text=player)
+        global current
+        current ='⭕' if current =='❌' else '❌'
+        # if buttons[button_id].configure(text=):
+        #     button.configure(foreground='black')
 
     # uses a recursive function to display the time and the after method allows you to wait a certain amouont of time for it to update 
     def count(sec):
+
         if sec>0:
             timer.configure(text=f"-----------------------------  Starting in {sec} seconds  -----------------------------")
             timer.after(1000,count,sec-1)
         else :
             timer.configure(text="--------------------------------  Game started  -------------------------------------")
-    
+            
     # start game
     def startGame():
         start.configure(state=ctk.DISABLED)
@@ -55,12 +64,31 @@ def ticTacToe():
         count(sec)
         for i in buttons:
             i.configure(state=ctk.ACTIVE)
-        return True
-
+        
+        user()
+        for i in buttons:
+            print(i)
     
     # this function will be what decides whos turn it is and what their value is x or o
     def user():
-        pass
+
+        
+        #this if statment say if first move == me it will go through the buttons and let the user pick first
+        if firstMove.get() == "Me":
+            for j ,(x,y)in  enumerate(positions):
+                
+                buttons[j].configure(command=lambda i=j:on_click(i,current))
+
+                
+                
+
+        if firstMove.get() == "Ai":
+            pass
+        else:
+            pass
+        
+            
+            
 
 
     #-------------------All functions go in above-------------------
@@ -83,10 +111,11 @@ def ticTacToe():
         (2,0),(2,1),(2,2)
     ] 
     
-    button_symbols = ['❌', '⭕', '❌', '⭕', '❌', '⭕', '❌', '⭕', '❌']
+   
   
     for i,( row ,column) in enumerate(positions):
-        button =ctk.CTkButton(master=frameButtons, text=' ', height=116, width=116, command=lambda i=i:on_click(i))
+        
+        button =ctk.CTkButton(master=frameButtons, text=' ', height=116, width=116)
         button.grid(padx=(10, 2) if column == 0 else (2, 2) if 0 < column < 2 else (2, 10), pady=(10, 2) if row == 0 else (2, 2) if 0 < row < 2 else (2, 10), row=row, column=column)
         buttons.append(button)
         buttons[i].configure(state=ctk.DISABLED)
@@ -114,7 +143,7 @@ def ticTacToe():
     ctk.CTkLabel(master=frameOther, font=('Agency FB', 26), text="Who moves first...              Click below to   ").pack(pady=(5,1))
 
     firstMove_var = ctk.StringVar(value="Random")
-    firstMove = ctk.CTkComboBox(master=frameOther, values=["Me", "The AI", "Random"],
+    firstMove = ctk.CTkComboBox(master=frameOther, values=["Me", "AI", "Random"],
                                     variable=firstMove_var,
                                     font=('Agency FB', 27)
                                     )

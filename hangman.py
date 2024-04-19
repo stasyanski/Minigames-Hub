98a2b9bbc06007917ba611f6fda1672e6e7efef8
stas <157709692+stasyanski@users.noreply.github.com>
2024-04-19 12:36:30 +0100

    Changed from AI to player 2 and player 1

--- hangman.py
+++ hangman.py
@@ -1,11 +1,11 @@
 """
 program: hangman.py
 author: Stanislav Birca, Kameron Bains
-
 this python file acts as the interface
 for the hangman minigame for our
 python minigame hub.
 """
+
 def hangman():
     #-------------------Libraries-------------------
     import customtkinter as ctk
--- ticTacToe.py
+++ ticTacToe.py
@@ -10,7 +10,7 @@ python minigame hub.
 def ticTacToe():  
     #-------------------Libraries-------------------
     import customtkinter as ctk
-    import random
+    from CTkMessagebox import CTkMessagebox
 
     #-------------------CTk appearance-------------------
     ctk.set_appearance_mode("Dark")
@@ -23,7 +23,7 @@ def ticTacToe():
 
     #-------------------Menu window-------------------
     tic = ctk.CTk() # Reference to ticTacToe window as tic 
-    window_height = 640; window_width = 400
+    window_height = 520; window_width = 400
     screen_width = tic.winfo_screenwidth()
     screen_height = tic.winfo_screenheight()
     center_x = int(screen_width/2 - window_width / 2)
@@ -38,23 +38,9 @@ def ticTacToe():
 
     # start game
     def startGame():
-        print('startgame func called')
-        for current in (start, difficulty, firstMove):
+        for current in buttons:
             current.configure(state=ctk.DISABLED)
         count(3) # starts timer countdown
-
-        # if the user starts it will call the user function first to give them their go 
-        if firstMove.get() == "Me":
-            user() # calls the ai function if user chose user
-            print('user func called')
-        elif firstMove.get() == "The AI":
-            ai() # calls the ai function if user chose the AI
-            print('ai func called')
-        else: 
-            print('random randint func called')
-            result = random.randint(1,2)
-            if result == 1: user(); 
-            else: ai()
 
 
     # end game
@@ -63,97 +49,59 @@ def ticTacToe():
         global end; end = True
         for i in buttons:
             i.configure(state=ctk.DISABLED)
+        timer.configure(text="--------------------------------  Game Over  -------------------------------------")
+        CTkMessagebox(tic, title='Game Over', message='Game Over!')
     
-    # function to check win conditions
+    # Function to check win conditions
     def check_winner():
         print('check_winner called')
-        pass
+
+        # Check rows
+        for i in range(3):
+            if buttons[i*3].cget('text') == buttons[i*3+1].cget('text') == buttons[i*3+2].cget('text') != ' ':
+                return end_game()
+
+        # Check columns
+        for i in range(3):
+            if buttons[i].cget('text') == buttons[i+3].cget('text') == buttons[i+6].cget('text') != ' ':
+                return end_game()
+
+        # Check diagonals
+        if buttons[0].cget('text') == buttons[4].cget('text') == buttons[8].cget('text') != ' ':
+            return end_game()
+        if buttons[2].cget('text') == buttons[4].cget('text') == buttons[6].cget('text') != ' ':
+            return end_game()
+
+        return None
 
     # Function to process click
     def on_click(button_id):
-        print('on click func called')
         global turn,current,end
 
         print(f"! Button {button_id} pressed !")
         buttons[button_id].configure(state=ctk.DISABLED, text=current,text_color_disabled='white',fg_color ='#539f7e')
-        
-        if not end:
-            # this will change current to the next turn and will check a winner 
-            if current == '❌':
-                current = '⭕'
-            else:
-                current = '❌'
-            
-            if check_winner():
-                pass
-            elif current == '⭕' :
-                ai()
-            elif current == '❌' :
-                user()
-            
-            """"
-            i tried implementing a better solution for changing turns, dont get rid of this yet
+        check_winner()
 
-            if firstMove.get() == 'Me':
-                if current == '❌':
-                    current = '⭕'
-                else:
-                    current = '❌'
-
-                if current == '❌':
-                    user()
-                
-            elif firstMove.get() == 'The AI':
-                if current == '❌':
-                    current = '⭕'
-                else:
-                    current = '❌'
-                
-                if current == '❌':
-                    ai()
-            """
+        if current == '❌':
+            current = '⭕'
+            timer.configure(text="--------------------------------  Player 2 Move  -------------------------------------")
+        else:
+            current = '❌'
+            timer.configure(text="--------------------------------  Player 1 Move  -------------------------------------")
                 
 
-    # uses a recursive function to display the time and the after method allows you to wait a certain amouont of time for it to update 
+    # Uses a recursive function to display the time and the after method allows you to wait a certain amouont of time for it to update 
     def count(sec):
         print('count func called')
         if sec>0:
             timer.configure(text=f"-----------------------------  Starting in {sec} seconds  -----------------------------")
             timer.after(1000,count,sec-1)
         else :
-            timer.configure(text="--------------------------------  Game started  -------------------------------------")
+            timer.configure(text="--------------------------------  Player 1 Move  -------------------------------------")
             for current in buttons:
                 current.configure(state=ctk.NORMAL, hover_color='#106a43') # sets buttons to active after timer
 
-
-    # function for the user to allow them to put their character where they want it 
-    def user():
-        print('user func called')
-        for j,(x,y) in enumerate(positions):
-            buttons[j].configure(command=lambda i=j:on_click(i))
-    
-
-    # ai function which uses a list comprehension to see the avliable spaces in the game 
-    def ai():
-        print('ai func called')
-        available_spots = [i for i, button in enumerate(buttons) if button.cget('text') == ' ']
-
-        #if there are any available places it uses the random choice function to select a spot 
-        if available_spots:
-            ai_choice = random.choice(available_spots)
-            on_click(ai_choice) # feeds ai choice into on click, where button is pressed
-        else:
-            end_game() # ends game if no available places
-
-
-    # function that will be used to assign difficulty
-    def difficulty_ai():
-        print('difficulty ai func called')
-        pass
-
-
     #-------------------All functions go in above-------------------
-        
 
 
 
@@ -172,13 +120,16 @@ def ticTacToe():
         (2,0),(2,1),(2,2)
     ] 
     
-    for i,( row ,column) in enumerate(positions):
+    for i,(row,column) in enumerate(positions):
         
         button =ctk.CTkButton(master=frameButtons, text=' ', height=116, width=116)
         button.grid(padx=(10, 2) if column == 0 else (2, 2) if 0 < column < 2 else (2, 10), pady=(10, 2) if row == 0 else (2, 2) if 0 < row < 2 else (2, 10), row=row, column=column)
         buttons.append(button)
         buttons[i].configure(state=ctk.DISABLED)
-
+    
+    # Function to assign signals to buttons
+    for j,(x,y) in enumerate(positions):
+        buttons[j].configure(command=lambda i=j:on_click(i))
     #-------------------Timer label-------------------
     timer = ctk.CTkLabel(master=tic, text="---------------------------------------   Timer   ---------------------------------------")
     timer.pack()
@@ -187,33 +138,9 @@ def ticTacToe():
     frameOther = ctk.CTkFrame(master=tic, width=400, height=400)
     frameOther.pack(padx=12, pady=0)
 
-    #-------------------Choose difficulty-------------------
-    ctk.CTkLabel(master=frameOther, font=('Agency FB', 42), text="Choose AI difficulty...").pack(pady=(10,1))
-
-    difficulty_var = ctk.StringVar(value="Intermediate")
-    difficulty = ctk.CTkSegmentedButton(master=frameOther, values=[" Beginner ", " Intermediate ", " Impossible "],
-                                                    variable=difficulty_var,
-                                                    font=('Agency FB', 27)
-                                                    )
-    difficulty.set(" Intermediate ")
-    difficulty.pack(padx=25, pady=(0,10))
-
-    #-------------------Who moves first-------------------
-    ctk.CTkLabel(master=frameOther, font=('Agency FB', 26), text="Who moves first...              Click below to   ").pack(pady=(5,1))
-
-    firstMove_var = ctk.StringVar(value="Random")
-    firstMove = ctk.CTkComboBox(master=frameOther, values=["Me", "The AI", "Random"],
-                                    variable=firstMove_var,
-                                    font=('Agency FB', 27),
-                                    state='readonly',
-                                    dropdown_font=('Agency FB',22)
-                                    )
-    firstMove.set("Random")
-    firstMove.pack(padx=25, pady=(0,20), side='left')
-
     # start button
     start = ctk.CTkButton(master=frameOther, font=('Agency FB', 27), text="Start Game",command=startGame)
-    start.pack(padx=25, pady=(0,20), side='right')
+    start.pack(padx=25, pady=(20,20), side='right')
 
     # Run the app
     tic.mainloop()
